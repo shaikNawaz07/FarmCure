6baa98e1308d9199270c49daa050bf50791e5d92
Nawaz <97388511+shaikNawaz07@users.noreply.github.com>
2025-05-17 23:21:12 +0530

    Update main.py

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 def model_prediction(test_image):
-    model = tf.keras.models.load_model('trained_plant_disease_model.keras')
+    model = tf.keras.models.load_model("PLANT-DISEASE-IDENTIFICATION/trained_plant_disease_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
